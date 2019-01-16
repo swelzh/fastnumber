8c8844dd45cdcac7486ecdc5e3bbc1fb8fd131ce
lizhihua495 <lizhihua495@pingan.com.cn>
2019-01-16 16:59:32 +0800

    UPDATE

--- FastNumber/RememberNumberController.m
+++ FastNumber/RememberNumberController.m
@@ -17,8 +17,8 @@
 #define SCREEN_WIDTH ([[UIScreen mainScreen] bounds].size.width)
 
 
-static int marginLeft = 20;
-static int marginRight = 20;
+static int marginLeft = 15;
+static int marginRight = 15;
 
 
 @interface RememberNumberController()
@@ -27,7 +27,7 @@ static int marginRight = 20;
 @property(strong,nonatomic) UICollectionView *collectionView;
 @property(strong,nonatomic) NSMutableArray *arr;
 @property(strong,nonatomic) IndexHelper *indexHelper;
-
+@property(strong,nonatomic) UIView *inputView;
 
 @end
 
@@ -53,23 +53,54 @@ static NSString * const reuseIdentifier = @"Cell";
         NSNumber *nubmer = @(arc4random_uniform(10));
         [self.arr addObject:nubmer];
     }
+    
+    [self addCollectionView];
+    
+}
+
+
+- (void)addCollectionView{
+    // addInputView
+    UIView *inputView = [UIView new];
+    inputView.backgroundColor = [UIColor yellowColor];
+    [self.view addSubview:inputView];
+    self.inputView = inputView;
+    
+    
+    
+    // addCollectionView
     UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc] init];
     flowLayout.minimumLineSpacing = 0;
     flowLayout.minimumInteritemSpacing = 0;
-    CGFloat itemWidth = (SCREEN_WIDTH - 20 -20) / kRowCount;
-    flowLayout.itemSize = CGSizeMake(itemWidth, itemWidth);
     
     [flowLayout setScrollDirection:UICollectionViewScrollDirectionVertical];
     self.collectionView = [[UICollectionView alloc] initWithFrame:[[UIScreen mainScreen] bounds] collectionViewLayout:flowLayout];
     [self.collectionView registerClass:[CollectionViewCell class] forCellWithReuseIdentifier:@"MyCollectionCell"];
+    self.collectionView.scrollEnabled = YES;
     
     self.collectionView.backgroundColor = [UIColor whiteColor];
     [self.view addSubview:self.collectionView];
-    
     self.collectionView.dataSource = self;
     self.collectionView.delegate = self;
     [self.collectionView registerClass:[CollectionReusableView class] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"Header"];
+    
+    
+    [self.collectionView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.left.right.equalTo(self.view);
+        make.bottom.equalTo(self.inputView.mas_top);
+    }];
+    
+    [inputView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.collectionView.mas_bottom);
+        make.left.bottom.right.equalTo(self.view);
+        make.height.equalTo(@(200));
+    }];
+    
+ 
+    
+    
 }
+
 
 #pragma mark - UICollectionViewDataSource
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
@@ -91,7 +122,7 @@ static NSString * const reuseIdentifier = @"Cell";
     CollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"MyCollectionCell" forIndexPath:indexPath];
  
     if ([self.indexHelper isRowEnd:indexPath]) {
-        cell.textLabel.text = [NSString stringWithFormat:@"Row%ld",(indexPath.row + 1)/(kRowCount + 1)];
+        cell.textLabel.text = [NSString stringWithFormat:@"row%ld",(indexPath.row + 1)/(kRowCount + 1)];
         cell.rightLine.hidden = YES;
     }else if (indexPath.row < self.arr.count) {
         cell.textLabel.text = [[self.arr objectAtIndex:indexPath.row] stringValue];
@@ -100,7 +131,6 @@ static NSString * const reuseIdentifier = @"Cell";
         cell.textLabel.text = @"";
         cell.rightLine.hidden = YES;
     }
-    
     return cell;
 }
 
@@ -122,6 +152,44 @@ static NSString * const reuseIdentifier = @"Cell";
     return reusable;
 }
 
+
+
+#pragma mark - UICollectionViewDelegateFlowLayout
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath{
+    
+    NSInteger rowCount = kRowCount;
+   
+    CGFloat displayWidth = SCREEN_WIDTH - marginLeft - marginRight;
+    CGFloat rowLabelWidth = 60;
+    
+    
+    
+    // 判断是否行末
+    BOOL isRowEnd = [self.indexHelper isRowEnd:indexPath];
+    
+    // 数字cell
+    CGFloat itemWidth = (displayWidth - rowLabelWidth) / rowCount;
+    CGFloat itemHeight = itemWidth+2;
+    CGSize itemSize = CGSizeMake(itemWidth, itemHeight);
+    
+    // 行数cell
+    if (isRowEnd) {
+        itemSize = CGSizeMake(rowLabelWidth, itemHeight);
+    }
+    
+    return itemSize;
+}
+
+
+- (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section{
+    return UIEdgeInsetsMake(20, marginLeft, 10, marginRight);
+    //    return UIEdgeInsetsMake(0, 0, 0, 0);
+}
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout referenceSizeForHeaderInSection:(NSInteger)section{
+    return CGSizeMake(300, 20);
+}
+
 #pragma mark - UICollectionViewDelegate
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
     
@@ -140,41 +208,7 @@ static NSString * const reuseIdentifier = @"Cell";
 
 
 
-#pragma mark - UICollectionViewDelegateFlowLayout
-- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath{
-    
-    NSInteger rowCount = kRowCount;
-    NSInteger cellCount = kRowCount + 1;
-    
-    CGFloat displayWidth = SCREEN_WIDTH - marginLeft - marginRight;
-    CGFloat rowLabelWidth = 70;
-    
-    
-    
-    // 判断是否行末
-    BOOL isRowEnd = [self.indexHelper isRowEnd:indexPath];
-    
-    // 数字cell
-    CGFloat itemWidth = (displayWidth - rowLabelWidth) / rowCount;
-    CGFloat itemHeight = itemWidth;
-    CGSize itemSize = CGSizeMake(itemWidth, itemWidth);
-    
-    // 行数cell
-    if (isRowEnd) {
-        itemSize = CGSizeMake(rowLabelWidth, itemHeight);
-    }
-    
-    return itemSize;
-}
 
-- (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section{
-    return UIEdgeInsetsMake(20, marginLeft, 10, marginRight);
-//    return UIEdgeInsetsMake(0, 0, 0, 0);
-}
-
-- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout referenceSizeForHeaderInSection:(NSInteger)section{
-    return CGSizeMake(300, 20);
-}
 /*
  
  - (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section;
